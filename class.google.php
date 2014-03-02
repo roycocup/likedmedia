3bdaef828168614e4ebfda9fef4d73af15df43f7
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-03-02 15:08:46 +0000

    more stuff

--- class.google.php
+++ class.google.php
@@ -22,52 +22,41 @@ class Goo {
 		$client->setClientSecret($this->config->getCred('secret'));
 		$client->setRedirectUri($this->config->getCred('callback'));
 		$client->setDeveloperKey($this->config->getCred('dev-key'));
+		$client->setScopes(array(
+			'https://www.googleapis.com/auth/calendar', 
+			)
+		);
 		return $client;
 	}
 
 	public function authenticate(){
 		$client = $this->getClient();
-		$service = new Google_Service_Calendar($client);
-		if (isset($_REQUEST['logout'])) {
-			unset($_SESSION['access_token']);
+		$cal = new Google_Service_Calendar($client);
+		
+
+		if (isset($_GET['logout'])) {
+			unset($_SESSION['token']);
 		}
 
 		if (isset($_GET['code'])) {
 			$client->authenticate($_GET['code']);
-			$_SESSION['access_token'] = $client->getAccessToken();
+			$_SESSION['token'] = $client->getAccessToken();
 			header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 		}
 
-		if (isset($_SESSION['access_token'])) {
-			$client->setAccessToken($_SESSION['access_token']);
+		if (isset($_SESSION['token'])) {
+			$client->setAccessToken($_SESSION['token']);
 		}
 
 		if ($client->getAccessToken()) {
-			$me = $plus->people->get('me');
+			$calList = $cal->calendarList->listCalendarList();
+			print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
 
-			// These fields are currently filtered through the PHP sanitize filters.
-			// See http://www.php.net/manual/en/filter.filters.sanitize.php
-			$url = filter_var($me['url'], FILTER_VALIDATE_URL);
-			$img = filter_var($me['image']['url'], FILTER_VALIDATE_URL);
-			$name = filter_var($me['displayName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-			$personMarkup = "<a rel='me' href='$url'>$name</a><div><img src='$img'></div>";
 
-			$optParams = array('maxResults' => 100);
-			$activities = $plus->activities->listActivities('me', 'public', $optParams);
-			$activityMarkup = '';
-			foreach($activities['items'] as $activity) {
-		    // These fields are currently filtered through the PHP sanitize filters.
-		    // See http://www.php.net/manual/en/filter.filters.sanitize.php
-				$url = filter_var($activity['url'], FILTER_VALIDATE_URL);
-				$title = filter_var($activity['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-				$content = filter_var($activity['object']['content'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-				$activityMarkup .= "<div class='activity'><a href='$url'>$title</a><div>$content</div></div>";
-			}
-
-  			// The access token may have been updated lazily.
-			$_SESSION['access_token'] = $client->getAccessToken();
+			$_SESSION['token'] = $client->getAccessToken();
 		} else {
 			$authUrl = $client->createAuthUrl();
+			print "<a class='login' href='$authUrl'>Connect Me!</a>";
 		}
 		
 	}
