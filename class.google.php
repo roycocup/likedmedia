fabcf70c0a59dd38c7a6881fcdcf45b535156f54
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-03-02 15:32:15 +0000

    more stuff

--- class.google.php
+++ class.google.php
@@ -8,20 +8,29 @@ require_once 'Google/Service/Calendar.php';
 class Goo {
 
 	//main configurations object
-	private $config; 
+	private $_config; 
 
+	//Authorizarion Url - endpoint for the initial request before getting the token.
+	private $_authUrl; 
+
+	//the oauth token (also stored in session)
+	private $_token; 
+
+
+
+	//construct
 	public function __construct(){
-		$this->config = new Config();
+		$this->_config = new Config();
 	}
 
 
 	public function getClient(){
 		$client = new Google_Client();
 		$client->setApplicationName("Calendar Demo with Google API");
-		$client->setClientId($this->config->getCred('ID'));
-		$client->setClientSecret($this->config->getCred('secret'));
-		$client->setRedirectUri($this->config->getCred('callback'));
-		$client->setDeveloperKey($this->config->getCred('dev-key'));
+		$client->setClientId($this->_config->getCred('ID'));
+		$client->setClientSecret($this->_config->getCred('secret'));
+		$client->setRedirectUri($this->_config->getCred('callback'));
+		$client->setDeveloperKey($this->_config->getCred('dev-key'));
 		$client->setScopes(array(
 			'https://www.googleapis.com/auth/calendar', 
 			)
@@ -32,36 +41,42 @@ class Goo {
 	public function authenticate(){
 		$client = $this->getClient();
 		$cal = new Google_Service_Calendar($client);
-		
 
+		//get me out of here
 		if (isset($_GET['logout'])) {
 			unset($_SESSION['token']);
 		}
 
+		//get the app code and exchange it for the oauth token
 		if (isset($_GET['code'])) {
 			$client->authenticate($_GET['code']);
-			$_SESSION['token'] = $client->getAccessToken();
-			header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+			$_SESSION['token'] = $this->_token = $client->getAccessToken();
+			// header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 		}
+
 
 		if (isset($_SESSION['token'])) {
 			$client->setAccessToken($_SESSION['token']);
 		}
 
+		//if I cant get an access token yet
 		if ($client->getAccessToken()) {
 			$calList = $cal->calendarList->listCalendarList();
-			print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
-
-
+			// print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
 			$_SESSION['token'] = $client->getAccessToken();
 		} else {
-			$authUrl = $client->createAuthUrl();
-			print "<a class='login' href='$authUrl'>Connect Me!</a>";
+			
+			$this->authUrl = $client->createAuthUrl();
 		}
-		
+
 	}
+	
 
 }
+
+?>
+
+
 
 
 
--- index.php
+++ index.php
@@ -3,5 +3,42 @@
 require 'class.google.php';
 
 
-$t = new Goo();
-$t->authenticate();
+$goo = new Goo();
+$goo->authenticate();
+
+
+?>
+
+
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Like Digital Media Demo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+	<link rel="shortcut icon" href="http://localhost/golders_green/img/favico.ico" />
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/main.css">
+	
+</head>
+<body>
+	<div class="jumbotron">
+		<?php if (!empty($authUrl)): ?>
+			<h1>Like Digital Media</h1>
+			<p>This is a simple app that enables you to see your google calendar</p>
+			<p><a class="btn btn-primary btn-lg" role="button">Begin!</a></p>
+		<?php else: ?>
+			<h1>Well this is not working too well is it?</h1>
+			<p>If you can't see a button below, that because something that should be working is not.... </p>
+		<?php endif; ?>
+
+	</div>
+</body>
+</html>
