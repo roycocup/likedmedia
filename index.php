0df8fb5c6fb40c637beae21ce65b02429326fe9a
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-03-02 17:04:12 +0000

    more stuff

--- index.php
+++ index.php
@@ -18,27 +18,25 @@ $client->setRedirectUri('http://likedmedia.rodderscode.co.uk');
 $client->setDeveloperKey('AIzaSyDPf-yxc-KOVzy8765STLmUAlPdrRtd6I8');
 $cal = new Google_Service_Calendar($client);
 if (isset($_GET['logout'])) {
-  unset($_SESSION['token']);
+	unset($_SESSION['token']);
 }
 
 if (isset($_GET['code'])) {
-  $client->authenticate($_GET['code']);
-  $_SESSION['token'] = $client->getAccessToken();
-  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+	$client->authenticate($_GET['code']);
+	$_SESSION['token'] = $client->getAccessToken();
+	header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 }
 
 if (isset($_SESSION['token'])) {
-  $client->setAccessToken($_SESSION['token']);
+	$client->setAccessToken($_SESSION['token']);
 }
 
 if ($client->getAccessToken()) {
-  $calList = $cal->calendarList->listCalendarList();
-  print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
-
-
-$_SESSION['token'] = $client->getAccessToken();
+	$calList = $cal->calendarList->listCalendarList();
+	print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
+	$_SESSION['token'] = $client->getAccessToken();
 } else {
-  $authUrl = $client->createAuthUrl();
-  print "<a class='login' href='$authUrl'>Connect Me!</a>";
+	$authUrl = $client->createAuthUrl();
+	print "<a class='login' href='$authUrl'>Connect Me!</a>";
 }
 
