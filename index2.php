78067720876bb8960e3104de1c9ae26d14697e47
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-03-02 19:35:55 +0000

    more stuff

--- index2.php
+++ index2.php
@@ -1,76 +1,39 @@
 <?php
-session_start();
 error_reporting(E_ALL); 
 ini_set( 'display_errors','1');
-require 'class.google.php';
+set_include_path("google-api-php-client-master/src/" . PATH_SEPARATOR . get_include_path());
+require_once 'Google/Client.php';
+require_once 'Google/Service/Calendar.php';
+session_start();
 
 
-$goo = new Goo();
-
-$authUrl = $goo->getAuthUrl(); 
-
-
-//get me out of here
+$client = new Google_Client();
+$client->setApplicationName("Google Calendar PHP Starter Application");
+$client->setClientId('699975976929.apps.googleusercontent.com');
+$client->setClientSecret('-SHbDcMUZ5xXvVW2xIaJ6jrV');
+$client->setRedirectUri('http://likedmedia.rodderscode.co.uk');
+$client->setDeveloperKey('AIzaSyDPf-yxc-KOVzy8765STLmUAlPdrRtd6I8');
+$cal = new Google_Service_Calendar($client);
 if (isset($_GET['logout'])) {
 	unset($_SESSION['token']);
 }
 
-$authenticated = false;
 if (isset($_GET['code'])) {
-	$authenticated = $goo->authenticate($_GET['code']);
+	$client->authenticate($_GET['code']);
+	$_SESSION['token'] = $client->getAccessToken();
+	header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 }
 
-if ($authenticated){
-	header('Location: http://' . $_SERVER['HTTP_HOST']);
+if (isset($_SESSION['token'])) {
+	$client->setAccessToken($_SESSION['token']);
 }
 
+if ($client->getAccessToken()) {
+	$calList = $cal->calendarList->listCalendarList();
+	print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";
+	$_SESSION['token'] = $client->getAccessToken();
+} else {
+	$authUrl = $client->createAuthUrl();
+	print "<a class='login' href='$authUrl'>Connect Me!</a>";
+}
 
-?>
-
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Like Digital Media Demo</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-	<link rel="shortcut icon" href="http://localhost/golders_green/img/favico.ico" />
-	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/main.css">
-	
-</head>
-<body>
-<?php 
-	// $_SESSION['token']['access_token'] =  'ya29.1.AADtN_Uez_O7MTOWZslvtVwPBHqskuZCoFFpkgbwZf6QznbIsfzKwafkCnnTToYm4JoqXw'; 
-	// $_SESSION['token']['token_type'] = 'Bearer';
-	// $_SESSION['token']['expires_in'] = 3599;
-	// $_SESSION['token']['created'] = 1393778107;
-?>
-	<?php if (empty($_SESSION)): ?>
-		<?php if (!empty($authUrl)): ?>
-			<div class="jumbotron">
-				<h1>Like Digital Media</h1>
-				<p>This is a simple app that enables you to see your google calendar</p>
-				<p><a href="<?php echo $authUrl; ?>" class="btn btn-primary btn-lg" role="button">Begin!</a></p>
-			<?php elseif(empty($_SESSION)): ?>
-				<h1>Sorry...</h1>
-				<p>If you can't see a button below, its because something is not working.... </p>
-			</div>
-		<?php endif; ?>
-
-	<?php else: ?>
-		<div class="container">
-			<div class="h1 text-center">Welcome to the calendar app</div>
-			<div class="h3">This is a tiny application just to show that I can actually plug into the google API and move things around</div>	
-			<div>
-				<?php var_dump($goo->getCalList()); ?>
-			</div>
-		</div>
-	<?php endif; ?>
-</body>
-</html>
